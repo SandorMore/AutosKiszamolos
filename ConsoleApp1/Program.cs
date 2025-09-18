e68da2fadbb7313d1993873329eadbe80d7988b6
Móré Sándor <more.sandor@mechwart.local>
2025-09-18 14:08:03 +0200

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Numerics;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static List<Settlement> Settlements = new List<Settlement>();
+        static void Main(string[] args)
+        {
+            Read("Settlements.txt");
+        }
+
+        static void Read(string filePath)
+        {
+            StreamReader sr = new StreamReader(filePath);
+            sr.ReadLine();
+            string line;
+            while((line = sr.ReadLine()) != null)
+            {
+                Settlements.Add(new Settlement(line));
+            }
+        }
+        static bool LookForWay(float fuel, Settlement[] settlements)
+        {
+            return false;
+        }
+    }
+}
---
+++ ConsoleApp1/Settlement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Settlement
+    {
+        private string name {  get; set; }
+        private uint population {  get; set; }
+        private int hasCharger { get; set; }
+        double xCoordinate {  get; set; }
+        double yCoordinate { get; set; }
+        public Settlement(string line)
+        {
+            string[] lines = line.Split(';');
+            this.name = lines[0];
+            this.population = uint.Parse(lines[1]);
+            this.hasCharger = int.Parse(lines[2]);
+            this.xCoordinate = double.Parse(lines[3]);
+            this.yCoordinate = double.Parse(lines[4]);
+        }
+    }
+}
